0dcdc65be764f9462cf350e44f957db08e35281e
Lucas Luciano <lucasrluciano23@gmail.com>
2024-09-24 19:05:30 -0300

    :sparkles: feat: implementar lógica para contar velas com altura máxima

--- src/main/java/BirthdayCandle.java
+++ src/main/java/BirthdayCandle.java
@@ -1,5 +1,17 @@
 public class BirthdayCandle {
     public int countMaxHeightCandles(int[] candleHeights) {
-        return 0;
+        int max = 0;
+        int count = 0;
+
+        for (int height : candleHeights) {
+            if (height > max) {
+                max = height;
+                count = 1;
+            } else if (height == max) {
+                count++;
+            }
+        }
+        return count;
     }
+
 }
